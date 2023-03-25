efa5d6127f28cad108467cd82427b063fe430d54
Shimol-Khan <shimol@teleme.co>
2023-03-25 09:05:00 +0800

    Added broadcast transactions to Dashboard.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   before_action :authenticate_user!, only: [:dashboard]
 
   def dashboard
+    all_broadcasts = Broadcast.where(user_id: current_user.id)
+    @broadcasts = Kaminari.paginate_array(all_broadcasts).page(params[:page]).per(25)
   end
 end
